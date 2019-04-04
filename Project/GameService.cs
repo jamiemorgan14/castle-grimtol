77c13aaf3342add98736dbc3f9fe2f498900cb93
Jamie <jmorgan3475@gmail.com>
2019-04-03 23:49:28 -0600

    Established basic and first base, can travel between the two.

--- Project/GameService.cs
+++ Project/GameService.cs
@@ -1,10 +1,90 @@
+using System;
 using System.Collections.Generic;
 using CastleGrimtol.Project.Interfaces;
+using CastleGrimtol.Project.Models;
 
 namespace CastleGrimtol.Project
 {
-    public class GameService : IGameService
-    {
+  public class GameService : IGameService
+  {
+    public Room CurrentRoom { get; set; }
+    public Player CurrentPlayer { get; set; }
+    public bool Playing { get; set; }
 
+    private void Initialize()
+    {
+      //create rooms
+      Room basic = new Room("Basic Training", "You're in basic training. The foundation of your career.  Go (north) to travel to your first base");
+      Room firstBase = new Room("First Base", "Your first base is where most of your work will take place");
+      //create relationships
+      basic.AddExit(Direction.north, firstBase);
+
+
+
+      CurrentRoom = basic;
+      Playing = true;
     }
+    public void StartGame()
+    {
+      Initialize();
+      while (Playing)
+      {
+        System.Console.WriteLine($"{CurrentRoom.Name}: {CurrentRoom.Description}");
+        System.Console.WriteLine("Enter a direction to navigate through your career!");
+        string playersChoice = Console.ReadLine();
+        CurrentRoom = (Room)CurrentRoom.TravelToRoom(Direction.north);
+      }
+    }
+
+
+    public void GetUserInput()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Go(string direction)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Help()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Inventory()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Look()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Quit()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Reset()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Setup()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void TakeItem(string itemName)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void UseItem(string itemName)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
 }
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -1,10 +1,42 @@
+using System;
 using System.Collections.Generic;
 using CastleGrimtol.Project.Interfaces;
 
 namespace CastleGrimtol.Project.Models
 {
-    public class Room : IRoom
-    {
+  public class Room : IRoom
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Item> Items { get; set; }
+    public Dictionary<Direction, IRoom> Exits { get; set; }
 
+    internal IRoom TravelToRoom(Direction playersChosenDirection)
+    {
+      if (Exits.ContainsKey(playersChosenDirection))
+      {
+        return Exits[playersChosenDirection];
+      }
+      System.Console.WriteLine("You can't go that way!");
+      return (IRoom)this;
     }
+    internal void AddExit(Direction direction, IRoom destination)
+    {
+      Exits.Add(direction, destination);
+    }
+    public Room(string name, string description, Item item = null)
+    {
+      Name = name;
+      Description = description;
+      Exits = new Dictionary<Direction, IRoom>();
+    }
+
+  }
+  public enum Direction
+  {
+    north,
+    south,
+    east,
+    west
+  }
 }
